a8be3b8bcc489e04ab2bed69cea3ce80c187b210
Bilal Mohammed <kurdo1ka@gmail.com>
2024-09-18 09:51:35 +0200

    added template

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 let menus = [
     {
         "disches": "Hawaiisalat",
+        "info": "gemischter Salat mit Vorderschinken, Käse, Ananas und Dressing",
         "prices": 7.90,
         "amounts": 1
     },
     {
         "disches": "Pizzabrot",
+        "info": "mit Tomatensauce, Sardellen und Knoblauch",
         "prices": 6.20,
         "amounts": 3
     },
     {
         "disches": "Pizza Margherita",
+        "info": "Tomaten, Käse",
         "prices": 8.20,
         "amounts": 5
     },
     {
         "disches": "Rigatoni al forno",
+        "info": "mit Vorderschinken, Champignons, Erbsen in Fleisch- und Tomatensahnesauce überbacken",
         "prices": 9.40,
         "amounts": 2
     },
     {
         "disches": "Insalata nizzarda",
+        "info": "gemischter Salat mit Vorderschinken, Käse, Ei, Thunfisch und Dressing",
         "prices": 9.80,
         "amounts": 4
     }
 ]
 
-
-
-// let disches = ["Hawaiisalat", "Pizzabrot", "Pizza Margherita", "Rigatoni al forno", "Insalata nizzarda"];
-
-// let prices = [7.90, 6.20, 8.20, 9.40, 9.80];
-
-// let amounts = [1, 3, 5, 2, 4];
-
-
-// function init() {
-//     render();
-// }
-
-
-// function render() {
-//     main();
-// }
-
-
 function init() {
     let menusRef = document.getElementById('content');
     menusRef.innerHTML= "";
-
     for (let indexMenus = 0; indexMenus < menus.length; indexMenus++) {
         const element = menus[indexMenus];
         
@@ -59,7 +44,8 @@ function init() {
             </div>
             <div id="menu-info">
                 <h3>${element.disches}</h3>
-                <p>preis: ${element.prices}€</p>
+                <p>${element.info}</p>
+                <p>preis: ${element.prices} €</p>
             </div>
         </div>
         `
