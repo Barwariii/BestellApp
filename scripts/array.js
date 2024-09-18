5169517925fee00b1fb45fbb502de9a9869368c5
Bilal Mohammed <kurdo1ka@gmail.com>
2024-09-18 15:14:34 +0200

    extra array datei

---
+++ scripts/array.js
@@ -0,0 +1,37 @@
+let menus = [
+    {
+        "dishes": "Hawaiisalat",
+        "info": "gemischter Salat mit Vorderschinken, Käse, Ananas und Dressing",
+        "prices": 7.90,
+        "amounts": 1
+    },
+    {
+        "dishes": "Pizzabrot",
+        "info": "mit Tomatensauce, Sardellen und Knoblauch",
+        "prices": 6.20,
+        "amounts": 3
+    },
+    {
+        "dishes": "Pizza Margherita",
+        "info": "Tomaten, Käse",
+        "prices": 8.20,
+        "amounts": 5
+    },
+    {
+        "dishes": "Rigatoni al forno",
+        "info": "mit Vorderschinken, Champignons, Erbsen in Fleisch- und Tomatensahnesauce überbacken",
+        "prices": 9.40,
+        "amounts": 2
+    },
+    {
+        "dishes": "Insalata nizzarda",
+        "info": "gemischter Salat mit Vorderschinken, Käse, Ei, Thunfisch und Dressing",
+        "prices": 9.80,
+        "amounts": 4
+    }
+]
+
+
+let basket = [
+
+]
